2bc7d0d15d69bf145df9a46089a7a140a834831c
dernewt <dernewt@gmail.com>
2024-10-22 21:00:31 -0400

    block fractional pennies

--- SupermarketPricing/Money.cs
+++ SupermarketPricing/Money.cs
@@ -3,5 +3,17 @@ namespace SupermarketPricing;
 
 public class Money : ValueOf<decimal, Money>
 {
+    protected override void Validate()
+    {
+        if (!TryValidate())
+            throw new FractionalPennyException(Value.ToString()) { };
+    }
+
+    protected override bool TryValidate()
+    {
+        return Value.Scale <= 2;
+    }
 
 }
+
+public class FractionalPennyException(string Message) : Exception(Message) { }
